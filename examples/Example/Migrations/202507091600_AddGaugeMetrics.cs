30518e3d8da8e68875f3fd68bf902f04048efe78
Pavel Kravtsov <kravtsovpo@gmail.com>
2025-08-04 23:39:41 +0700

    Add .cs migration example

---
+++ examples/Example/Migrations/202507091600_AddGaugeMetrics.cs
@@ -0,0 +1,25 @@
+[ClickHouseMigration(202507091600, "AddGaugeMetrics")]
+public class AddGaugeMetrics : ClickHouseMigration
+{
+	protected override void Up(ClickHouseMigrationBuilder migrationBuilder)
+	{
+		migrationBuilder.AddRawSqlStatement("""
+			create table if not exists metrics_gauge (
+				name				LowCardinality(String),
+				labels				Map(LowCardinality(String), String),
+				help				LowCardinality(String),
+				value				Int64,
+				date_time			DateTime('UTC')			materialized now('UTC'),
+				ver					UInt32					materialized toUnixTimestamp(date_time),
+			)
+			engine = ReplacingMergeTree(ver)
+			primary key (name, labels)
+			order by (name, labels)
+		""");
+	}
+
+	protected override void Down(ClickHouseMigrationBuilder migrationBuilder)
+	{
+		migrationBuilder.AddRawSqlStatement("drop table if exists metrics_gauge");
+	}
+}
